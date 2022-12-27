bc980219928204358ce90c64aa50206fc4c9e943
diegosantosouza <diegosantos.s@hotmail.com>
2022-12-27 20:30:45 -0300

    feat: person

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const message = "hello word!"
-console.log(message)
+import * as express from "express"
+import { Request, Response } from "express"
+import Person from "@/person"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(new Person().sayHello())
+})
+
+app.listen(3000, () => console.log("listening on port 3000!"))
