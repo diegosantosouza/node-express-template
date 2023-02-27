080b1748751efe6f11736880fac373d48352bc3c
diegosantosouza <diegosantos.s@hotmail.com>
2023-02-27 19:32:42 -0300

    feat: mongoose

---
+++ src/infrastructure/db/mongodb/mongo-helper.ts
@@ -0,0 +1,30 @@
+import mongoose, { Collection, Connection } from 'mongoose'
+
+class MongoHelper {
+  connection: Connection | null
+
+  constructor(private readonly url: string) {
+    this.url = url
+    this.connection = null
+  }
+
+  async connect() {
+    await mongoose.connect(this.url,)
+    this.connection = mongoose.connection
+  }
+
+  async disconnect() {
+    await mongoose.disconnect()
+    this.connection = null
+  }
+
+  getCollection(name: string): Collection | null {
+    if (!this.connection) {
+      console.error('Database not connected')
+      return null
+    }
+    return this.connection.collection(name)
+  }
+}
+
+export default MongoHelper
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import app from './app'
+import MongoHelper from './infrastructure/db/mongodb/mongo-helper'
 
 const port = process.env.PORT || 3000
+const uri = process.env.MONGO_URI || 'mongodb://localhost/node-template'
+
+try {
+  new MongoHelper(uri).connect()
+} catch (error) {
+  console.error('unable to connect to DB. Error: ', error)
+}
 
 app.listen(port, () => console.info(`Server initialized on port: ${port}`))
