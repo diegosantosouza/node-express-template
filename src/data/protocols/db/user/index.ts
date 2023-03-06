0b1b8ff9cc72a2923aa4361a24b677be9a28b0e4
diegosantosouza <diegosantos.s@hotmail.com>
2023-03-06 16:59:25 -0300

    feat: index protocol

---
+++ src/data/protocols/db/user/index.ts
@@ -0,0 +1,5 @@
+export * from './create-user-repository'
+export * from './index-user-repository'
+export * from './remove-user-repository'
+export * from './show-user-repository'
+export * from './update-user-repository'
