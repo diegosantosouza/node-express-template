5fdae0d4960d3fecaf8c1a60dc6dc44c26299fcb
diegosantosouza <diegosantos.s@hotmail.com>
2023-03-14 22:20:09 -0300

    feat: mongo memory server

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   roots: ['<rootDir>/tests'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest',
