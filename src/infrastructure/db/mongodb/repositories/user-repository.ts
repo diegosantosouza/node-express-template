cd1e2d81e105403736ccad2928cfb26ec03b24a0
diegosantosouza <diegosantos.s@hotmail.com>
2023-03-24 18:07:52 -0300

    refactor: mongo helper refactor

--- src/infrastructure/db/mongodb/repositories/user-repository.ts
+++ src/infrastructure/db/mongodb/repositories/user-repository.ts
@@ -2,12 +2,14 @@ import { UserModel } from '@/domain/models/user';
 import { CreateUserRepository, IndexUserRepository, RemoveUserRepository, ShowUserRepository, UpdateUserRepository } from "@/data/protocols/db/user"
 import MongoHelper from "../mongo-helper"
 import { UserDoc, UserSchema } from "../schemas/user"
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/user/load-account-by-email-repository';
 
 export class UserRepository implements CreateUserRepository,
   RemoveUserRepository,
   UpdateUserRepository,
   ShowUserRepository,
-  IndexUserRepository {
+  IndexUserRepository,
+  LoadAccountByEmailRepository {
   async create(data: CreateUserRepository.Params): Promise<CreateUserRepository.Result> {
     const newUser = await UserSchema.create(data)
     return newUser.toObject()
@@ -19,12 +21,12 @@ export class UserRepository implements CreateUserRepository,
   }
 
   async show(id: ShowUserRepository.Params): Promise<ShowUserRepository.Result> {
-    const user = await UserSchema.findById(id)
+    const user = await UserSchema.findById(id).select('-password')
     return user ? user.toObject() : null
   }
 
   async update({ id, data }: UpdateUserRepository.Params): Promise<UpdateUserRepository.Result> {
-    const user = await UserSchema.findByIdAndUpdate(id, data, { new: true })
+    const user = await UserSchema.findByIdAndUpdate(id, data, { new: true }).select('-password')
     return user ? user.toObject() : null
   }
 
@@ -32,6 +34,11 @@ export class UserRepository implements CreateUserRepository,
     const select = { select: '-password' }
     const users = await MongoHelper.paginate<UserDoc, UserModel>(params.where, UserSchema, { ...params, ...select })
     return users
+  }
+
+  async loadByEmail(email: string): Promise<LoadAccountByEmailRepository.Result> {
+    const user = await UserSchema.findOne({ email })
+    return user ? user.toObject() : null
   }
 
 }
