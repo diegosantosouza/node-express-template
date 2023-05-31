474caf87957887a9cfd63882e9db168fc6ee38b3
diegosantosouza <diegosantos.s@hotmail.com>
2023-05-31 19:17:28 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import * as express from 'express'
 import setupMiddlewares from '@/main/config/middlewares'
 import setupRoutes from '@/main/config/routes'
+import setupSwagger from './config-swagger'
 import { Express } from 'express'
 
 export const setupApp = async (): Promise<Express> => {
   const app = express()
+  setupSwagger(app)
   setupMiddlewares(app)
   setupRoutes(app)
   return app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'node-express-template',
+    description: 'Boilerplate for projects',
+    version: '1.0.0',
+  }
+}
