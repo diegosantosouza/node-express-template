e8ab556a090a4115c6a16c59a8f723bee567470c
diegosantosouza <diegosantos.s@hotmail.com>
2023-09-06 23:50:28 -0300

    feat: graceful shutdown

---
+++ src/main/enums/exit-status-enum.ts
@@ -0,0 +1,4 @@
+export enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,42 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import { setupApp } from '@/main/config/app'
 import MongoHelper from '@/infrastructure/db/mongodb/mongo-helper'
+import { ExitStatus } from './main/enums/exit-status-enum'
 
 const port = process.env.PORT || 3000
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err)
+  process.exit(ExitStatus.Failure)
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error(`Unhandled Rejection promise: ${promise} and reason ${reason}`)
+  throw reason
+})
 
 async function startServer() {
   try {
     await MongoHelper.connect()
     const app = await setupApp()
-    app.listen(port, () => {
+    const server = app.listen(port, () => {
       console.info(`Server initialized on port: ${port}`)
     })
+
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+    exitSignals.map((sig) => process.on(sig, async () => {
+      try {
+        server.close()
+        console.info('Server exited with success')
+        process.exit(ExitStatus.Success)
+      } catch (error) {
+        console.error('Server exited with error:', error)
+        process.exit(ExitStatus.Failure)
+      }
+    }))
   } catch (error) {
     console.error('Failed to start server:', error)
+    process.exit(ExitStatus.Failure)
   }
 }
 
